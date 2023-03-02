f8c4fdfd755108fd19d77489cee3843826647a45
Juan Martin Fernandez <juanmartin7961992@gmail.com>
2023-03-02 13:58:47 -0300

    Force user dark theme

--- app/src/main/java/com/simon/materialCCPExample/ui/theme/Theme.kt
+++ app/src/main/java/com/simon/materialCCPExample/ui/theme/Theme.kt
@@ -52,7 +52,7 @@ fun MaterialCCPExampleTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
         darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        else -> DarkColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
