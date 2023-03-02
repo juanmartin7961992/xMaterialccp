cbb694032e99f60ceec16ea3c0a0618c317d7748
Juan Martin Fernandez <juanmartin7961992@gmail.com>
2023-03-02 14:05:59 -0300

    Changes

--- app/src/main/java/com/simon/materialCCPExample/ui/theme/Theme.kt
+++ app/src/main/java/com/simon/materialCCPExample/ui/theme/Theme.kt
@@ -22,23 +22,6 @@ private val DarkColorScheme = darkColorScheme(
     error = RED800,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-    error = RED800,
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
-
 @Composable
 fun MaterialCCPExampleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
